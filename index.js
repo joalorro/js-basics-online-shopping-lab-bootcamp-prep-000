c07236665b49d91db7f64cf08550914482da77f3
Josue V. Alorro <joalorro@gmail.com>
2018-06-12 02:16:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function addToCart(item) {
 
 function viewCart() {
   var msg = "In your cart, you have"
-  var currentItem
+  var currentItem = cart[0]
   if (cart.length === 0 ){
     return 'Your shopping cart is empty.'
   } else if (cart.length === 1){
