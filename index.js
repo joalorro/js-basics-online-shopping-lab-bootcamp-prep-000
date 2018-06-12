186e814bd83a02a40b710d3be644aeec3fd68636
Josue V. Alorro <joalorro@gmail.com>
2018-06-12 02:23:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,13 +20,14 @@ function addToCart(item) {
 function viewCart() {
   var msg = "In your cart, you have"
   var currentItem = cart[0]
+  
   if (cart.length === 0 ){
     return 'Your shopping cart is empty.'
   } else if (cart.length === 1){
     return `${msg} ${currentItem['itemName']} at $${currentItem["itemPrice"]}.`
   } else if (cart.length === 2){
     return `${msg} ${cart[0]['itemName']} at $${cart[0]["itemPrice"]}, and ${cart[1]['itemName']} at $${cart[1]['itemPrice']}.`
-  } else if (cart.length > 2){
+  } else {
     
     for (let i = 0; i < cart.length; i++){
       currentItem = cart[i]
@@ -37,6 +38,7 @@ function viewCart() {
         msg = `${msg}and ${currentItem.itemName} at $${currentItem.itemPrice}.`
       }
       
+      return msg
     }
     
   }
