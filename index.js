7408e795ba0575364186b8e53a9c8bd2c6e8bf68
Josue V. Alorro <joalorro@gmail.com>
2018-06-12 02:05:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var price = Math.random * 100
+  var price = (Math.random * 100 + 1)
   var newItem = {itemName: item, itemPrice: price}
   cart.push(newItem)
   return `${newItem['itemName']} has been added to your cart.`
