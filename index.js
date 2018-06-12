466b2b06875359f3f91f4944df0dcca57e16ca1a
Josue V. Alorro <joalorro@gmail.com>
2018-06-12 02:50:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,8 +77,8 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === '' || cardNumber){
-    return 
+  if (cardNumber === ''){
+    return "Sorry, we don't have a credit card on file for you."
   }
 }
 
